18d14e92fb8959720e2563d6ed6085f557b57b93
Samuel Cruz Gonzalez <cruzgonzalezsamuel@gmail.com>
2023-05-04 10:38:44 -0500

    image change

--- resources/views/admin/bookings/show.blade.php
+++ resources/views/admin/bookings/show.blade.php
@@ -117,7 +117,7 @@
 	</nav>
     
 
-	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(../../event/images/pareja-2.jpeg);" data-stellar-background-ratio="0.5">
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(../../event/images/pareja-5.jpeg);" data-stellar-background-ratio="0.5">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row">
